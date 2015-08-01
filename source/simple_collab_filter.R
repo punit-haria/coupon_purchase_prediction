b7bdbf13c78e1981d0afe65482791bf93f9f2c5c
punit_shah@bitbucket.com <punit_shah@bitbucket.org>
2015-08-01 15:01:30 -0700

    Adding references.

--- source/simple_collab_filter.R
+++ source/simple_collab_filter.R
@@ -2,6 +2,20 @@ library(dplyr)
 
 load("data/base.RData")
 
-# generate coupon ratings for each user
+# idea: build a model
+# X --> Y
+# where X is item + user features and Y is user rating
+# then, pick top 10 items for each user
+
+
+raw_coupon_vars <- c("CAPSULE_TEXT", "GENRE_NAME", "PRICE_RATE", "CATALOG_PRICE",
+                     "DISCOUNT_PRICE", "large_area_name", "ken_name", "small_area_name")
+
+raw_user_vars <- c("SEX_ID", "AGE", "PREF_NAME")
+
+
+
+
+
 
 
