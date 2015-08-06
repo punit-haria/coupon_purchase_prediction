8abeefc40773f1f2717d60d40125b927e16d3d8d
Punit Shah <punit@Punits-MacBook-Pro.local>
2015-08-05 18:32:00 -0700

    More refactoring

--- source_py/model.py
+++ source_py/model.py
@@ -1,95 +1,87 @@
 from source_py.data import DataLoader
-from source_py.item_profile import ItemProfile
+from source_py.item import ItemProfile
+from source_py.user import User
 
 import pandas as pd
 
 
-class User(object):
+class Model(object):
 
-    def __init__(self, loader, index, item_profile):
+    def __init__(self, loader):
         """
-        :param loader: DataLoader object with access to all data
-        :param index: row index into user_list DataFrame for this user
-        :param item_profile : ItemProfile object
+        :param loader: DataLoader
         """
+        assert isinstance(loader, DataLoader)
         self.data = loader
-        # this user
-        self.user = self.data.user_list.ix[index]
-        self.index = index
-        # transactions
-        self.purchases = self.data.details_train[self.data.details_train.USER_ID_hash == self.user.USER_ID_hash]
-        # purchased coupons
-        self.coupons = self.data.coupons_train[self.data.coupons_train.COUPON_ID_hash.isin(self.purchases.COUPON_ID_hash)]
-        # item profile object
-        self.item_profile = item_profile
+
+    def run(self, start, end):
+        """
+        :param start: start index in training set
+        :param end: end index in training set
+        The entire model training process is done using this subset of the training data.
+        """
+        raise NotImplementedError
+
+    def predict(self, start, end, train=True):
+        """
+        :param start: start index in DataFrame
+        :param end: end index in DataFrame
+        Predictions are made for rows in [start:end). If train is True, then the training
+        coupon dataset is used. Otherwise, the test coupon dataset is used.
+        """
+        raise NotImplementedError
 
 
-    def get_id(self):
+class ContentFilter(Model):
+
+    def __init__(self, loader):
         """
-        :return: ID of this user
+        :param loader: DataLoader
         """
-        return self.user.USER_ID_hash
+        super(ContentFilter, self).__init__(loader)
 
 
-    def recommend(self):
-        """
-        :return: gets the recommended coupons from the test set for this user
-        """
-        # get similarity scores for each test coupon
-        scores = self.item_profile.similarity(self.coupons.index, self.data.coupons_test.index)
-        # x-axis: test coupons, y-axis: user coupons
-        scores = scores.transpose()
-        # compute mean similarity score for each test coupon
-        scores["mean"] = scores.mean(axis=1)
-        # sort by descending order of mean score
-        scores.sort(columns="mean", ascending=False, inplace=True)
-        # get top test coupon indices
-        top = User.num_coupons()
-        if top < 1:
-            return ""
-        top_indices = scores.head(n=top).index
-        # get top coupon IDs
-        coups = self.data.coupons_test.ix[top_indices].COUPON_ID_hash.tolist()
-        # return as space-delimited string
-        ids = ""
-        for value in coups:
-            ids += value + " "
-        return ids
+    def run(self, start, end):
+        submission = []
+
+        load = DataLoader()
+        item_profile = ItemProfile(load)
+
+        print "Userlist size: ", load.user_list.shape
+
+        e = 0
+        for index in load.user_list.index:
+            user = User(load, index, item_profile)
+            coupons = user.recommend()
+            submission.append([user.get_id(), coupons])
+            e += 1
+            if e % 1000 == 0:
+                print "At User: ", e
+
+        return pd.DataFrame(submission, columns=["USER_ID_hash", "PURCHASED_COUPONS"])
 
 
-    @staticmethod
-    def num_coupons():
-        """
-        :return: the number of coupons to recommend for this user
-        """
-        return 5
+    def predict(self, start, end, train=True):
+        super(ContentFilter, self).predict(start, end, train)
+
 
 
 def run(output_filename):
-    submission = []
-
     load = DataLoader()
-    item_profile = ItemProfile(load)
-
-    print "Userlist size: ", load.user_list.shape
-
-    e = 0
-    for index in load.user_list.index:
-        user = User(load, index, item_profile)
-        coupons = user.recommend()
-        submission.append([user.get_id(), coupons])
-        e += 1
-        if e % 1000 == 0:
-            print "At User: ", e
-    final_df = pd.DataFrame(submission, columns=["USER_ID_hash", "PURCHASED_COUPONS"])
-
+    model = ContentFilter(load)
+    final_df = model.run(0,100)
     final_df.to_csv(output_filename, sep=",", index=False, header=True)
 
 
-
 if __name__ == '__main__':
-
     run('submissions/testing.csv')
+
+
+
+
+
+
+
 
 
 
---
+++ source_py/run.py
@@ -0,0 +1,10 @@
+from source_py.data import DataLoader
+from source_py.model import Model
+
+
+if __name__ == '__main__':
+
+    load = DataLoader()
+    val = Model(load)
+    #run('submissions/testing.csv')
+
