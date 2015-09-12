991c3772caceea1007f5bd163a146ecb9d5c6ae2
Punit <punit_shah@bitbucket.org>
2015-09-12 09:17:27 -0700

    Update

---
+++ source_py/visual/tsne.py
@@ -0,0 +1,51 @@
+__author__ = 'Punit'
+
+from collections import OrderedDict
+from time import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+import seaborn as sns
+
+from sklearn.preprocessing import StandardScaler
+from scipy.linalg import svd
+from sklearn.decomposition import TruncatedSVD
+
+from sklearn.manifold import TSNE
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from source_py.data import DataLoader
+
+sns.set(style="darkgrid", palette="muted")
+pd.set_option('display.mpl_style', 'default')
+pd.set_option('display.notebook_repr_html', True)
+plt.rcParams['figure.figsize'] = 10, 6
+np.random.seed(0)
+
+def plot_3d_scatter(A, elevation=30, azimuth=120):
+    """ Create 3D scatterplot """
+
+    maxpts=1000
+    fig = plt.figure(1, figsize=(9, 9))
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=elevation, azim=azimuth)
+    ax.set_xlabel('component 0')
+    ax.set_ylabel('component 1')
+    ax.set_zlabel('component 2')
+
+    # plot subset of points
+    rndpts = np.sort(np.random.choice(A.shape[0], min(maxpts,A.shape[0]), replace=False))
+    coloridx = np.unique(A.iloc[rndpts]['class'], return_inverse=True)
+    colors = coloridx[1] / len(coloridx[0])
+
+    sp = ax.scatter(A.iloc[rndpts,0], A.iloc[rndpts,1], A.iloc[rndpts,2]
+               ,c=colors, cmap="jet", marker='o', alpha=0.6
+               ,s=50, linewidths=0.8, edgecolor='#BBBBBB')
+
+    plt.show()
+
+
+load = DataLoader()
