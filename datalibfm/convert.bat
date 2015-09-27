a2db49518ac67a69625db75a1774a2aad8b11fd9
Punit <punit_shah@bitbucket.org>
2015-09-27 00:56:51 -0700

    update

---
+++ datalibfm/convert.bat
@@ -0,0 +1,2 @@
+libfm-1.40.windows\convert.exe -ifile datalibfm\train_output.txt -ofilex datalibfm\train_bin.x -ofiley datalibfm\train_bin.y
+libfm-1.40.windows\convert.exe -ifile datalibfm\test_output.txt -ofilex datalibfm\test_bin.x -ofiley datalibfm\test_bin.y
---
+++ datalibfm/transpose.bat
@@ -0,0 +1,2 @@
+libfm-1.40.windows\transpose.exe -ifile datalibfm\train_bin.x -ofile datalibfm\train_bin.xt
+libfm-1.40.windows\transpose.exe -ifile datalibfm\test_bin.x -ofile datalibfm\test_bin.xt
