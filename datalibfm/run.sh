28388467cea3761e55b89aa240174108dabcf1d8
Punit <punit_shah@bitbucket.org>
2015-09-26 23:09:18 -0700

    update

--- datalibfm/run.sh
+++ datalibfm/run.sh
@@ -1,1 +1,1 @@
-./libfm-1.42.src/bin/libFM -task r -train datalibfm/train_output.txt -test datalibfm/test_output.txt -dim '1,1,10' -out datalibfm/libfm_output.txt
+./libfm-1.42.src/bin/libFM -task r -train datalibfm/train_output.txt -test datalibfm/test_output.txt -dim '1,1,10' -out datalibfm/libfm_output.txt -method als -regular 0.1
