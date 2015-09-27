39c1990fde6ce0175f8a50846dae7197a9b755e1
Punit Shah <punit@Punits-MacBook-Pro.local>
2015-09-27 10:32:02 -0700

    .

--- datalibfm/conversion_script.sh
+++ datalibfm/conversion_script.sh
@@ -1,3 +1,4 @@
 ipython -m source_py.libfm_input datalibfm/raw_train_output.txt datalibfm/raw_test_output.txt
+echo "removing quotes..."
 sed 's/\"/ /g' datalibfm/raw_train_output.txt > datalibfm/train_output.txt 
 sed 's/\"/ /g' datalibfm/raw_test_output.txt > datalibfm/test_output.txt 
