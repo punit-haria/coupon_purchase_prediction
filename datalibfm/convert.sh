b2147bece67f7137c0240a7af64ad4bee67298ca
Punit <punit_shah@bitbucket.org>
2015-09-27 10:29:57 -0700

    update

---
+++ datalibfm/convert.sh
@@ -0,0 +1,2 @@
+./libfm-1.42.src/bin/convert -ifile datalibfm/train_output.txt -ofilex datalibfm/train_bin.x -ofiley datalibfm/train_bin.y
+./libfm-1.42.src/bin/convert -ifile datalibfm/test_output.txt -ofilex datalibfm/test_bin.x -ofiley datalibfm/test_bin.y
---
+++ datalibfm/transpose.sh
@@ -0,0 +1,2 @@
+./libfm-1.42.src/bin/transpose -ifile datalibfm/train_bin.x -ofile datalibfm/train_bin.xt
+./libfm-1.42.src/bin/transpose -ifile datalibfm/test_bin.x -ofile datalibfm/test_bin.xt
